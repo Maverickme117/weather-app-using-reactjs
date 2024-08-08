59b2fc89a4da03158c09830f5ed0d9285f56c180
Maverickme117 <163622603+Maverickme117@users.noreply.github.com>
2024-08-08 13:15:07 +0530

    Add files via upload

---
+++ React-Weather-App-main/React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="#"
+        > Rahul</a
+      >, <a href="https://github.com/Maverickme117/weather-app.git">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
